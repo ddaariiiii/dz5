f5390b97255b8c88bf2079125d6e5732c8b4983b
pc_digit <oushen.ms@gmail.com>
2025-04-25 17:22:05 +0200

    update. hello dari user

---
+++ script.js
@@ -0,0 +1,30 @@
+async function getPosts () {
+    
+    try {
+        const response = await fetch ( 'https://jsonplaceholder.typicode.com/posts' );
+        
+        //  корректность ответа
+        if ( !response.ok ) {
+            throw new Error ( 'HTTP request - response is bad.' );
+        }
+        
+        // парсинг JSON-данных
+        const posts = await response.json ();
+        
+        // Первые 5 постов для отображения
+        console.log ( 'First 5 posts:', posts.slice (0, 5) );
+        
+        // Для отображения постов на странице
+        const postList = document.getElementById ( 'post-list' );
+        posts.slice ( 0, 5 ).forEach ( post => {
+           
+           const li = document.createElement ( 'li' );
+           li.textContent = `${post.title} (ID: ${post.id})`;
+           postList.appendChild ( li );
+        });
+    } catch ( error ) {
+        console.error ( 'Error in get posts', error );
+    }
+}
+
+getPosts ();
